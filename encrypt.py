f1cb82774937edbe15e1b4caf033e59b4bc1925b
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-01-24 20:25:00 +0800

    Delete encrypt.py

--- encrypt.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-__import__("dotenv").load_dotenv()
-
-try:
-    print(''.join(map(lambda x: chr(ord(x)+int(os.environ.get("ENCRYPT"))), [*sys.argv[1]])))
-except Exception as e:
-    print(e)
-    print("Usage: python encrypt.py [string]") or exit()
