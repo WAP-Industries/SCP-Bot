830a0f2da9b92cc66b4746cde23a5828e235f8f1
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-01-25 08:12:43 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,7 +8,16 @@ class SCPBot:
     async def on_ready():
         print("im running nigger")
 
-    @Bot.command()
+    @Bot.command(help="closes the bot")
     async def exit(ctx):
         await ctx.send("bye nigger")
         await SCPBot.Bot.close()
+
+
+class CustomHelp(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        command_list = [*mapping.values()][0]
+        longest = max(map(lambda x:len(x.name), command_list))
+        await self.get_destination().send("```{}```".format('\n'.join([f'{i.name:<{longest+5}}{i.short_doc}' for i in command_list])))
+        
+SCPBot.Bot.help_command = CustomHelp()
